1033fb02c8201763fd713c32bc5ae0044af5c267
Witkowski Antoine <witkowski.antoine@gmail.com>
2022-04-04 20:46:38 +0200

    add NEXT header && update favicon

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <>
+            <Head>
+                <title>Bon API - Vietnamese food</title>
+                <meta name="description" content="Bon API Vietnamese food"/>
+                <link rel="icon" href="./ant-favicon.jpg"/>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp;
