a2deda8c693937460b25f2a82bb2d50857f85654
Alexandre TO <alexandreto1996@gmail.com>
2022-04-04 17:01:19 +0200

    init Tailwind

--- front/pages/index.js
+++ front/pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+            <p className='font-bold underlind'>Hello World !</p>
         </p>
 
         <div className={styles.grid}>
