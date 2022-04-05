db98ef1cc15ea863b1554f6270245823101fab19
Kris Bang <dc.ambang@ecole-ipssi.net>
2022-04-05 17:18:29 +0200

    Create plat commande

---
+++ api/src/Controller/PlatCommandeController.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Controller;
+
+use App\Model\PlatCommandeModel;
+use Core\Controller\DefaultController;
+
+final class PlatCommandeController extends DefaultController{
+
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new PlatCommandeModel;
+    }
+
+    /**
+     * @return void
+     */
+    public function getAll()
+    {
+        $platsCommande = $this->model->findAll();
+        $this->jsonResponse($platsCommande, 200);
+    }
+
+    /**
+     * @param int $id
+     * @return void
+     */
+    public function getOne(int $id)
+    {
+        $platCommande = $this->model->find($id);
+        if ($plat) {
+            $this->jsonResponse($platCommande, 200);
+        } else {
+            $this->jsonResponse("Cette commande n'existe pas", 404);
+        }
+    }
+
+    /**
+     * @param array $data
+     * @return void
+     */
+    public function save(array $data)
+    {
+        $lastId = $this->model->save($data);
+        $platCommande = $this->model->find($lastId);
+        $this->jsonResponse($platCommande, 201);
+    }
+
+    /**
+     * @param int $id
+     * @param array $data
+     * @return void
+     */
+    public function update(int $id, array $data) {
+        $this->model->update($id, $data);
+        $this->jsonResponse("Commmande modifié", 201);
+    }
+
+    /**
+     * @param int $id
+     * @return void
+     */
+    public function delete(int $id) {
+        $platCommande = $this->model->find($id);
+        if ($platCommande) {
+            $this->model->delete($id);
+            $this->jsonResponse("Commande supprimé", 200);
+        } else {
+            $this->jsonResponse("Cette commande n'existe pas/plus", 200);
+        }
+    }
+}
