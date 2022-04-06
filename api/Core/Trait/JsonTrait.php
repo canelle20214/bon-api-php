0681163b1cac2a1b74c04c816259bcb2632892d6
Witkowski Antoine <witkowski.antoine@gmail.com>
2022-04-06 17:43:29 +0200

    update headers (for CORS)

--- api/Core/Trait/JsonTrait.php
+++ api/Core/Trait/JsonTrait.php
@@ -14,6 +14,8 @@ trait JsonTrait {
     {
         header("Content-type: application/json");
         header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET POST PUT DELETE OPTIONS");
+        header("Access-Control-Allow-Credentials: true");
         http_response_code($responseCode);
 
         echo json_encode($data);
